109cf76a3752d5d3f0a0e542575cc623864a9f9c
Samuel LaPrade <sam@csm-wl-dhcp-238-71.mines.edu>
2022-12-01 11:34:56 -0700

    i think i got the hsv to work properly

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import numpy as np
 
 # array of boundaries
 bounds = np.array([
-    np.array([10, 0, 200]), np.array([30, 255, 255]), # white boundaries
-    np.array([35, 100, 0]), np.array([55, 255, 255]), # green boundaries
+    np.array([10, 0, 200]), np.array([40, 70, 255]), # white boundaries
+    np.array([35, 0, 0]), np.array([55, 255, 255]), # green boundaries
     np.array([170, 100, 0]), np.array([190, 255, 255]), # red boundaries
     np.array([75, 100, 0]), np.array([95, 255, 255]), # blue boundaries
-    np.array([0, 100, 0]), np.array([10, 255, 255]), # orange boundaries
-    np.array([15, 100, 0]), np.array([35, 255, 255]) # yellow boundaries
+    np.array([0, 180, 0]), np.array([10, 255, 255]), # orange boundaries
+    np.array([15, 180, 0]), np.array([35, 255, 255]) # yellow boundaries
 ])
 # solved cube
 solved = [
@@ -24,8 +24,6 @@ def processImage(bgr_img):
     processed_img = np.zeros((bgr_img.shape[0], bgr_img.shape[1], 3), dtype="uint8") # value to return
     hsv_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
     for i in range(6): # loop through for all 6 colors
-        if i != 0:
-            continue
         mask = cv2.inRange(hsv_img, bounds[i*2], bounds[i*2+1]) # grab mask for specific range
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10)) # kernel to clean up vertical divisions
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel) # morphological open
